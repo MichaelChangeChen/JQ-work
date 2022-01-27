2ce1c4e1abaa9ec7a38d2b631a0e546e3d4c2dda
Michael Chen <michaelexgt@gmail.com>
2022-01-27 16:00:27 +0800

    upload

--- Mynode/index.js
+++ Mynode/index.js
@@ -1,29 +1,113 @@
 //NODE_ENV 在.env裡 是可以自己取名子做為不同的
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 //抓.env的內容
 require('dotenv').config();
-console.log(require('dotenv').config());
+// console.log(require('dotenv').config());
 // 1. 引入express
 const express = require('express');
+
+
+//呼叫multer
+const multer = require('multer');
+//dest目的地
+const upload = multer({dest:'tmp_uploads/'})
 
 // 2. 建立web server 物件
 const app = express();
 
 // 註冊樣版引擎
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+
+//middleware 是這支urlencoded  (夜店保安,一開始就先過濾)
+app.use(express.urlencoded({extended:false}));
+//第二次過濾 只接受json
+app.use(express.json());
 
 //放在所有路由設定的前面
 app.use(express.static('public'));
 
 // 3. 路由設定  只接受GET的方法
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     //render()呈現樣板
-    res.render('home',{name:'Michael'});
+    res.render('home', { name: 'Michael' });
 
 });
 
+//這段不太明白-------------------------
+app.get('/a/b', (req, res) => {
+    //render()呈現樣板
+    res.render('home', { name: 'Michael' });
+});
+//這段不太明白-------------------------
+
+//-------------json-sales--------------
+//抓取  json陣列
+app.get('/json-sales', (req, res) => {
+    //require進來時就已經會是陣列
+    //放入sales
+    const sales = require('./data/sales');
+    // const sales = [];
+
+
+    // if((req.query.orderByCol= 'age') && (req.query.orderByRule='desc')){
+    //     console.log('oookkk')
+    //     sales = sale.age.sort((a,b)=> a-b);
+    //     res.render('json-sales',{sales})
+    // }else{
+    //     console.log('nnnoooo')
+    // }
+    // TODO:排序
+    //req.query.orderByCol = age
+    //req.query.orderByRule = desc
+    const col = req.query.orderByCol
+    const rule = req.query.orderByRule
+
+
+    console.log(req.query);
+    // res.render('json-sales',{sales})
+    // console.log(sales); 
+    //send()的是傳字串
+    // res.send(sales[1].name);
+    res.render('json-sales',{sales});
+});
+//----------------------------------------
+
+
+
+///-------------try-qs----------query
+app.get('/try-qs', (req, res) => {
+    res.json(req.query);
+})
+///------------------------------
+
+///--------------try-post----------
+app.post('/try-post', (req, res) => {
+    res.json(req.body);
+})
+///--------------------------------
+
+
+//---------/try-post-form-------------
+
+app.get('/try-post-form', (req, res)=>{
+    res.render('try-post-form');
+});
+app.post('/try-post-form', (req, res)=>{
+    res.render('try-post-form', req.body);
+});
+//-------------------------------------
+
+
+//----------------try-upload-----------------
+app.post('/try-upload',upload.single('avatar'), (req, res)=>{
+    // res.json(req.body);
+    res.json(req.file);
+});
+
+//-------------------------------------
+
 //只接受USE的方法
-app.use((req,res)=>{
+app.use((req, res) => {
     res.status(404).send('<h1 style="color:red">404唷~</h1>');
 })
 
@@ -31,6 +115,6 @@ app.use((req,res)=>{
 const port = process.env.PORT || 3001;
 
 // 4. Server 偵聽
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`啟動server 偵聽埠號 ${port} - `, new Date());
 });
